deab157de2f5cb9aae32d5b76057336d1ba40e61
InventedMost <netvid00@gmail.com>
2022-05-18 23:26:52 +0100

    chess rows and colunm

--- src/App.js
+++ src/App.js
@@ -62,14 +62,132 @@ function App() {
     // }
     // console.log( `result : ${sum}(${array[0]},${array[1]},${array[2]})`)
     // console.log(tileObj,wordObj)
-    console.log(wordObj.c)
+
   };
 
-  match(word, tile,obj);
+  // match(word, tile,obj);
 
   // console.log(array, sum);
 
   //  Product.test()
+  let number = []
+  var twoSum = function(nums, target) {
+      for(let i = 0 ; i < nums.length; i++){
+           for(let j = 0 ; j < nums.length; j++){
+               if(nums[i] + nums[j] === target){
+                  //  number.push[j]
+                  //  number.push[i]
+                  console.log(nums[j], nums[i])
+                  console.log(j,i)
+               }
+        }
+      }
+      
+  };
+
+  // twoSum([1,2,3,4], 5)
+// let output = []
+// let now
+//   var addTwoNumbers = function(l1, l2) {
+//     for(let i = 0 ; i < l1 ; i++){
+//         // now = l1[i] + l2[i]
+//         console.log(firstarray)
+        // output.push(now)
+//     }
+//     // console.log(output) 
+// };
+
+// addTwoNumbers([2,4,3],[5,6,4])
+
+// let output = []
+// const addTwoNumbers = (l1,l2) => {
+//   console.log(l1,l2)
+//   for(let i = 0 ; i < l1.length ; i++){
+//      output.push(l1[i] + l2[i])
+//   }
+//   console.log(output.reverse())
+// }
+
+// addTwoNumbers([2,4,3],[5,6,2])
+
+
+let newArr = []
+let sumForArr = 0
+let result = 0
+
+const findMedianSortedArrays =(nums1, nums2) => {
+    for(let i = 0 ; i < nums1.length; i++){
+      newArr.push(nums1[i])
+    }
+
+    for(let i = 0 ; i < nums2.length; i++){
+      newArr.push(nums2[i])
+    }
+
+    for(let i = 0 ; i < newArr.length; i++){
+       sumForArr += newArr[i]
+    }
+
+    result = sumForArr / newArr.length
+    console.log(result)
+};
+
+findMedianSortedArrays([1,2],[3,4])
+
+
+const longestPalindrome = (s) => {
+    let splittedS = s.split("")
+    console.log(splittedS)
+    for(let i = 0; i < splittedS.length; i++){
+      console.log("first")
+      for(let j = 0; j < splittedS.length; i++){
+         if( splittedS[i] === splittedS[j] && i !== j){
+             console.log(i,j)
+            //  console.log('here')
+             console.log(splittedS[i],splittedS[j])
+         }
+      }
+    }
+};
+
+
+// longestPalindrome("heyy")
+
+const zigGetter = (s, row ) => {
+  const test = []
+  let zigColunmOne = 0
+  let paced 
+  let pacedTwo
+  const stringLength = s.length
+  const refinedString = s.split("")
+  zigColunmOne = row + (row -2 )
+  let remainder = stringLength % zigColunmOne
+  const zigColunmEnd = (stringLength - remainder) / zigColunmOne 
+  paced = 1 + (row - 2)
+  let colunms = 1 + (zigColunmEnd * paced)
+  console.log(refinedString) 
+  for(let i = 0; i < row; i++){
+      console.log(refinedString[2])
+      test.push(refinedString[i])
+  }
+  for(let i = 0; i < row; i++){
+      refinedString.shift()
+  }
+
+  console.log("colunm", zigColunmOne)
+  console.log("colunmEnd", zigColunmEnd)
+  console.log("stringl", stringLength)
+  console.log("remaider", remainder)
+  console.log("paced", paced)
+  console.log("colunms",colunms)
+  console.log(refinedString)
+  console.log("test", test)
+}
+
+zigGetter("paypalishiring", 5)
+
+
+
   return (
     <>
       <button className="App" onClick={Product.test}>
