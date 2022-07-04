e2432c86baff5e6e60ddeb2c02e1fb1d3a0fc95b
InventedMost <netvid00@gmail.com>
2022-07-04 18:16:54 +0100

    today

--- src/App.js
+++ src/App.js
@@ -2274,23 +2274,82 @@ function App() {
     let left = 0;
     let right = nums.length - 1;
     let mid;
-    let min;
 
     while (left < right) {
       mid = Math.floor((right + left) / 2);
 
-      if (nums[left] >= nums[right]) {
-        min = nums[right];
+      if (nums[mid] >= nums[left]) {
         left = mid + 1;
       } else {
-        min = nums[left];
         right = mid - 1;
       }
     }
-    return min;
+    return mid;
   };
 
-  console.log(findMin([2, 4, 5, 6, 7, 0, 1]));
+  // console.log(findMin([2, 4, 5, 6, 7, 0, 1]));
+
+  const binarySearchTarget = (nums, target) => {
+    let l = 0;
+    let r = nums.length - 1;
+    let m;
+
+    while (l <= r) {
+      m = Math.floor((l + r) / 2);
+
+      if (nums[m] > target) r = m - 1;
+      else if (nums[m] < target) l = m + 1;
+      else {
+        return m;
+      }
+    }
+    return -1;
+  };
+
+  // binarySearchTarget([-1, 0, 3, 5, 9, 12], 9)
+
+  const solve2dMatrix = (matrix, target) => {
+    let temp;
+    let l = 0;
+    let r;
+    let m;
+
+    for (let a of matrix) {
+      r = a.length - 1;
+      if (a[r] >= target) {
+        temp = a;
+        break;
+      }
+    }
+
+    if (!temp) return false;
+
+    while (l <= r) {
+      m = Math.floor((l + r) / 2);
+      if (temp[m] > target) r = m - 1;
+      else if (temp[m] < target) l = m + 1;
+      else {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  // console.log(
+  //   solve2dMatrix(
+  //     [
+  //       [1, 3, 5, 7],
+  //       [10, 11, 16, 20],
+  //       [23, 30, 34, 60],
+  //     ],
+  //     1
+  //   )
+  // );
+
+  const kokoEatsBanana = (piles, hours) => {
+    console.log(Math.max(piles));
+  };
+  kokoEatsBanana([3, 6, 7, 11], 8);
 
   return (
     <>
